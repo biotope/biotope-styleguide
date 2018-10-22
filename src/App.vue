02a780c2d59e353c40b2118d55f20b6f6f35e4bc
Christoph Hummel <christoph.hummel@virtual-identity.com>
2018-10-22 14:43:55 +0200

    init vue commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <styleGuide></styleGuide>
+</template>
+
+<script>
+import StyleGuide from './components/styleGuide';
+
+export default {
+  components: {
+    StyleGuide
+  }
+}
+</script>
