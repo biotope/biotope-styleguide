0a2d93cb134f27992a998190af2e0eabe056df0e
VI-Lukas <lukas.gorniak@virtual-identity.com>
2020-01-03 13:00:48 +0100

    Feature/fmt 344 (#12)
    * change markup to account for new width
    
    * new build
    
    * changed markup
    
    * markup update
    
    * markup changes
    
    * markup changes
    
    * markup and css changes
    
    * changed markup
    
    * add anchor markup to template
    
    * added anchor links
    
    * change scroll id selection
    
    * add newest build
    
    * add handle classes to scroll element
    
    * use vue scroll event
    
    * change button to link
    
    * change href link
    
    * change top anchor to header
    
    * small cleanup to trigger build

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,225 +1,261 @@
 <template>
   <div class="styleGuide__overview">
-    <ul class="styleGuide__category">
+    <div class="styleGuide__filter">
+      <ul class="styleGuide__category">
         <li class="styleGuide__categoryItem" v-bind:class="{ 'is-active': (activeCategory === '')}">
-            <a @click="setActiveCategory()" href="#">{{ $t('overview_category_all') }}</a>
+          <a @click="setActiveCategory()" href="#">{{ $t('overview_category_all') }}</a>
         </li>
-        <li v-for="category in categories" class="styleGuide__categoryItem" v-bind:class="{ 'is-active': (activeCategory === category)}" :key="category">
-            <a @click="setActiveCategory(category)" href="#">{{ category }}</a>
+        <li
+          v-for="category in categories"
+          class="styleGuide__categoryItem"
+          v-bind:class="{ 'is-active': (activeCategory === category)}"
+          :key="category"
+        >
+          <a @click="setActiveCategory(category)" href="#">{{ category }}</a>
         </li>
+      </ul>
+      <div class="styleGuide__searchWrapper">
+        <input
+          class="styleGuide__search"
+          type="search"
+          v-model="searchString"
+          autofocus
+          :placeholder="$t('overview_search_placeholder')"
+        />
+      </div>
+    </div>
+    <ul class="styleGuide__sort">
+      <li class="styleGuide__sortItem" v-for="item in listOfSort.slice(0, 16)" :key="item">
+        <button
+          :disabled="isSortItemDisabled(item)"
+          v-scroll-to="{ el: '#styleGuide-' + item}"
+        >{{item}}</button>
+      </li>
+      <li class="styleGuide__sortItem" v-for="item in listOfSort.slice(16, 26)" :key="item">
+        <button
+          :disabled="isSortItemDisabled(item)"
+          v-scroll-to="{ el: '#styleGuide-' + item}"
+        >{{item}}</button>
+      </li>
     </ul>
-    <div class="styleGuide__searchWrapper">
-        <input class="styleGuide__search" type="search" v-model="searchString" autofocus :placeholder="$t('overview_search_placeholder')">
+    <div
+      :id="'styleGuide-' + index"
+      v-for="(componentList,index) in filteredComponentList"
+      :key="index"
+    >
+      <h3 class="styleGuide__letter">
+        {{index}}
+        <a href="#" class="ui-icon-pfeilhoch" v-scroll-to="{ el: '#header'}">Nach oben</a>
+      </h3>
+      <ul class="styleGuide__items">
+        <li v-for="component in componentList" class="styleGuide__item" :key="component.name">
+          <router-link
+            :to="{ name: 'details', params: { name: component.name }}"
+          >{{ component.keywords[0] }}</router-link>
+        </li>
+      </ul>
     </div>
-    <div class="styleGuide__sort">
-        <span class="styleGuide__sortItem"  v-for="item in listOfSort.slice(0, 13)" :key="item">
-            <button :disabled="isSortItemDisabled(item)" v-scroll-to="{ el: '#styleGide-' + item}">{{item}}</button>
-        </span>
-    </div>
-    <div class="styleGuide__sort">
-        <span class="styleGuide__sortItem" v-for="item in listOfSort.slice(13, 26)" :key="item">
-            <button :disabled="isSortItemDisabled(item)" v-scroll-to="{ el: '#styleGide-' + item}">{{item}}</button>
-        </span>
-    </div>
-    <div :id="'styleGide-' + index" v-for="(componentList,index) in filteredComponentList" :key="index">
-        <h3 class="styleGuide__letter">{{index}}</h3>
-        <ul class="styleGuide__items">
-            <li v-for="component in componentList" class="styleGuide__item" :key="component.name">
-                <router-link :to="{ name: 'details', params: { name: component.name }}">{{ component.keywords[0] }}</router-link>
-            </li>
-        </ul>
-    </div>
-    <div v-if="Object.getOwnPropertyNames(filteredComponentList).length == 0" class="styleGuide__noResult">{{ $t('overview_noResults') }}</div>
+    <div
+      v-if="Object.getOwnPropertyNames(filteredComponentList).length == 0"
+      class="styleGuide__noResult"
+    >{{ $t('overview_noResults') }}</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'styleGuide',
+  name: "styleGuide",
 
-  data () {
+  data() {
     return {
       activeListOfSort: [],
       categories: [],
-      activeCategory: '',
-      listOfSort: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
-      searchString: '',
-    }
+      activeCategory: "",
+      listOfSort: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
+      searchString: ""
+    };
   },
   computed: {
-      filteredComponentList: function() {
-        const matches = (searchString, packageJsonItem) => packageJsonItem.name.toLowerCase().match(searchString.toLowerCase()) || (packageJsonItem.keywords || []).some(tag => tag.toLowerCase().match(searchString.toLowerCase()));
+    filteredComponentList: function() {
+      const matches = (searchString, packageJsonItem) =>
+        packageJsonItem.name.toLowerCase().match(searchString.toLowerCase()) ||
+        (packageJsonItem.keywords || []).some(tag =>
+          tag.toLowerCase().match(searchString.toLowerCase())
+        );
 
-        let currentObject = this.groupComponents(this.$store.getters.getComponentList);
+      let currentObject = this.groupComponents(
+        this.$store.getters.getComponentList
+      );
 
-        let newObject = {};
-        let activeListOfSort = [];
-        let categories = this.categories;
-        for(const sortArray in currentObject) {
-            currentObject[sortArray].forEach((item) => {
-                if(categories.indexOf(item.category) === -1) {
-                    categories.push(item.category);
-                }
-                if(this.activeCategory === '' || this.activeCategory === item.category) {
-                    if(matches(this.searchString, item)) {
-                        if (!newObject[sortArray]) {
-            
-                            activeListOfSort.push(sortArray);
-                            newObject[sortArray] = [];
-                        }
-                        newObject[sortArray].push(item);
-                    }
-                }
-            });
-        }
-        this.setactiveListOfSort(activeListOfSort);
-        this.setCategories(categories);
-        let sorted = Object.keys(newObject)
-            .sort()
-            .reduce((acc, key) => ({
-                ...acc, [key]: newObject[key].sort(function(a, b) {
-                    if (a.keywords[0] < b.keywords[0])
-                        return -1;
-                    if (a.keywords[0] > b.keywords[0])
-                        return 1;
-                    return 0;
-                })
-            }), {});
-        return sorted;
-      },
-
+      let newObject = {};
+      let activeListOfSort = [];
+      let categories = this.categories;
+      for (const sortArray in currentObject) {
+        currentObject[sortArray].forEach(item => {
+          if (categories.indexOf(item.category) === -1) {
+            categories.push(item.category);
+          }
+          if (
+            this.activeCategory === "" ||
+            this.activeCategory === item.category
+          ) {
+            if (matches(this.searchString, item)) {
+              if (!newObject[sortArray]) {
+                activeListOfSort.push(sortArray);
+                newObject[sortArray] = [];
+              }
+              newObject[sortArray].push(item);
+            }
+          }
+        });
+      }
+      this.setactiveListOfSort(activeListOfSort);
+      this.setCategories(categories);
+      let sorted = Object.keys(newObject)
+        .sort()
+        .reduce(
+          (acc, key) => ({
+            ...acc,
+            [key]: newObject[key].sort(function(a, b) {
+              if (a.keywords[0] < b.keywords[0]) return -1;
+              if (a.keywords[0] > b.keywords[0]) return 1;
+              return 0;
+            })
+          }),
+          {}
+        );
+      return sorted;
+    }
   },
 
   methods: {
-      setactiveListOfSort: function(newActiveListOfSort) {
-            this.activeListOfSort = newActiveListOfSort;
-      },
+    setactiveListOfSort: function(newActiveListOfSort) {
+      this.activeListOfSort = newActiveListOfSort;
+    },
 
-      setCategories: function(newCategories) {
-            this.categories = newCategories;
-      },
+    setCategories: function(newCategories) {
+      this.categories = newCategories;
+    },
 
-      setActiveCategory: function(categoryString) {
-          this.activeCategory = categoryString || '';
-      },
+    setActiveCategory: function(categoryString) {
+      this.activeCategory = categoryString || "";
+    },
 
-      isSortItemDisabled: function(item) {
-          return !(this.activeListOfSort.indexOf(item) > -1);
-      },
+    isSortItemDisabled: function(item) {
+      return !(this.activeListOfSort.indexOf(item) > -1);
+    },
 
-      groupComponents: function(componentsList) {
-        let groupedComponentList = {};
-        componentsList.forEach(component => {
-            const firstLetter = component.keywords[0].charAt(0).toUpperCase();
-             if (!groupedComponentList[firstLetter]) {
-                groupedComponentList[firstLetter] = [];
-            }
-            groupedComponentList[firstLetter].push(component);
-        });
-        return groupedComponentList;
-      }
-   }
-}
-
+    groupComponents: function(componentsList) {
+      let groupedComponentList = {};
+      componentsList.forEach(component => {
+        const firstLetter = component.keywords[0].charAt(0).toUpperCase();
+        if (!groupedComponentList[firstLetter]) {
+          groupedComponentList[firstLetter] = [];
+        }
+        groupedComponentList[firstLetter].push(component);
+      });
+      return groupedComponentList;
+    }
+  }
+};
 </script>
 
 <style lang="scss">
-    /* styleGuide - layout */
-    $styleGuide-max-width: 1280px;
-    /* styleGuide - colors */
-    $styleGuilde-color-primary: #337ab7;
-    /* styleGuide - fonts */
-    $styleGuide-font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-    $styleGuide-font-color: #333;
-     /* styleGuide - links*/
-    $styleGuide-link-color: $styleGuilde-color-primary;
-    /* styleGuide - meta infos */
-    $styleGuide-meta-color: #ccc;
+/* styleGuide - layout */
+$styleGuide-max-width: 1280px;
+/* styleGuide - colors */
+$styleGuilde-color-primary: #337ab7;
+/* styleGuide - fonts */
+$styleGuide-font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+$styleGuide-font-color: #333;
+/* styleGuide - links*/
+$styleGuide-link-color: $styleGuilde-color-primary;
+/* styleGuide - meta infos */
+$styleGuide-meta-color: #ccc;
 
-    .styleGuide {
-        font-family: $styleGuide-font-family;
-        color: $styleGuide-font-color;
-        margin: 0 auto;
-        max-width: $styleGuide-max-width;
+.styleGuide {
+  font-family: $styleGuide-font-family;
+  color: $styleGuide-font-color;
+  margin: 0 auto;
+  max-width: $styleGuide-max-width;
 
-        a {
-            color: $styleGuide-link-color;
-        }
+  a {
+    color: $styleGuide-link-color;
+  }
 
-        &__category {
-            padding: 20px 0;
-        
-        }
-        &__categoryItem {
-            display: inline-block;
-            padding: 0 15px;
+  &__category {
+    padding: 20px 0;
+  }
+  &__categoryItem {
+    display: inline-block;
+    padding: 0 15px;
 
-            &:first-of-type {
-                padding: 0 15px 0 0;
-            }
-
-            &:last-of-type {
-                padding: 0 0 0 15px ;
-            }
-
-            &.is-active a {
-                color: #000;
-                cursor: default;
-                text-decoration: none;
-            }
-        }
-
-        &__search {
-            margin: 10px 0;
-            padding: 5px;
-            width: 100%;
-        }
-
-        &__sort{ 
-            display: flex;
-        }
-        &__sortItem {
-            flex: 1;
-            padding: 15px;
-
-            button {
-                background: none !important;
-                border: none; 
-                padding: 0 !important;
-                font: inherit;
-                cursor: pointer;
-                text-decoration: underline;
-            }
-
-            button[disabled] {
-                cursor: default;
-                text-decoration: none;
-            }
-        }
-
-        &__letter {
-            border-bottom: 1px solid gray;
-        }
-
-        &__filter {
-            &.styleGuide__filter--active {
-                color: red;
-            }
-        }
-        &__items {
-            padding: 0;
-        }
-
-        &__item {
-            padding: 0 0 40px;
-            list-style-type: none;
-
-            a {
-                display: block;
-            }
-            &:first-letter {
-                text-transform: capitalize;
-            }
-        }
+    &:first-of-type {
+      padding: 0 15px 0 0;
     }
+
+    &:last-of-type {
+      padding: 0 0 0 15px;
+    }
+
+    &.is-active a {
+      color: #000;
+      cursor: default;
+      text-decoration: none;
+    }
+  }
+
+  &__search {
+    margin: 10px 0;
+    padding: 5px;
+    width: 100%;
+  }
+
+  &__sort {
+    list-style-type: none;
+    text-align: left;
+  }
+  &__sortItem {
+    display: block;
+
+    button {
+      background: none !important;
+      border: none;
+      padding: 0 !important;
+      font: inherit;
+      cursor: pointer;
+      text-decoration: underline;
+    }
+
+    button[disabled] {
+      cursor: default;
+      text-decoration: none;
+    }
+  }
+
+  &__letter {
+    border-bottom: 1px solid gray;
+  }
+
+  &__filter {
+    &.styleGuide__filter--active {
+      color: red;
+    }
+  }
+  &__items {
+    padding: 0;
+  }
+
+  &__item {
+    padding: 0 0 40px;
+    list-style-type: none;
+
+    a {
+      display: block;
+    }
+    &:first-letter {
+      text-transform: capitalize;
+    }
+  }
+}
 </style>
